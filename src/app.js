74da367a0fd82f08d6edd83e3528d3a3dc2a3b1e
Jacin Montava <jmontava@gmail.com>
2020-07-28 08:29:04 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Fingerprint2 from '../node_modules/fingerprintjs2'
+
+Fingerprint2.get(function (components) {
+    console.log(components) // an array of components: {key: ..., value: ...}
+    var values = components.map(function (component) { return component.value })
+    var murmur = Fingerprint2.x64hash128(values.join(''), 31)
+    console.log(murmur);
+})
